7faae3ac8b4225ab047798616ac8db4b4169a118
Ignacio687 <ignadavidchaves@gmail.com>
2022-04-12 14:29:49 -0300

    cambioDiccionariosARepo

--- ejerciciosClase/Diccionarios/funcion_repetidos.py
+++
@@ -1,9 +0,0 @@
-from itertools import count
-
-def buscarRepetidos(list):
-    diccionarioRepetidos = {}
-    for value in list:
-        if value not in diccionarioRepetidos:
-            diccionarioRepetidos[value] = list.count(value)
-    return diccionarioRepetidos
-    
