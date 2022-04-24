c46d96cb37b8f376bb018fb6e0ae80fb2dd93cf6
Ignacio687 <ignadavidchaves@gmail.com>
2022-04-23 23:00:32 -0300

    codePracRecursividadListasTestsOk

--- ejerciciosClase/pracRecursividadListas/classList.py
+++ ejerciciosClase/pracRecursividadListas/classList.py
@@ -1,4 +1,5 @@
 
+from ast import Break
 from operator import truediv
 from pickletools import read_unicodestring1
 
@@ -7,16 +8,29 @@ class SearchList():
     def searchProcedural(self, list, value):
         if len(list) == 0:
             return False
-        start = 0
-        end = len(list)
-        while list[start] or list[end] == value or start == end or start == (end-1):
-            if list[end//2-value] == value:
-                return True
-                break
-            elif list[(end-start)//2] > value:
-                end = (end//2)-1
-            elif list[end//2-start] < value:
-                start = (end//2-start)+1
+        while len(list) >1:
+            if list[round(len(list)/2)-1]<value:
+                list = list[round(len(list)/2):]
+            elif list[round(len(list)/2)-1]>=value:
+                    list = list[0:round(len(list)/2)]
+        if list[0]==value:
+            return True 
+        else:
+            return False
+
+
+
+
+        # start = 0
+        # end = len(list)
+        # while list[start] or list[end] == value or start == end or start == (end-1):
+        #     if list[end//2-value] == value:
+        #         return True
+        #         break
+        #     elif list[(end-start)//2] > value:
+        #         end = (end//2)-1
+        #     elif list[end//2-start] < value:
+        #         start = start+(end//2-start)+1
 
 
 # hacer esto recursivo
--- ejerciciosClase/pracRecursividadListas/tests.py
+++ ejerciciosClase/pracRecursividadListas/tests.py
@@ -10,11 +10,20 @@ class ValueSearchTestCase(unittest.TestCase):
         self.assertTrue(self.list.searchProcedural([1], 1))
     def test_OneValueNotFound(self):
         self.assertFalse(self.list.searchProcedural([1], 2))
-    def test_MultipleValues(self):
+    def test_MultipleValuesMax(self):
         self.assertTrue(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 12))
-    def test_MultipleValues(self):
+    def test_MultipleValuesNotFound(self):
         self.assertFalse(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 18))
-
+    def test_MultipleValuesMin(self):
+        self.assertTrue(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 2))
+    def test_MultipleValuesPosition0(self):
+        self.assertTrue(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 1))
+    def test_MultipleValuesFinalPosition(self):
+        self.assertTrue(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 14))
+    def test_MultipleValuesMixSliceMax(self):
+        self.assertTrue(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 9))
+    def test_MultipleValuesMixSliceMin(self):
+        self.assertTrue(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 5))
 
 if __name__=='__main__':
     unittest.main()
