e74432bc5937a01a5354f56ce05c5ade8180efa1
Ignacio687 <ignadavidchaves@gmail.com>
2022-04-24 01:55:46 -0300

    recursiveAndProceduralCodeFromPracRecursividadListasPassAllTest

--- ejerciciosClase/pracRecursividadListas/classList.py
+++ ejerciciosClase/pracRecursividadListas/classList.py
@@ -11,11 +11,24 @@ class SearchList():
         while len(list) >1:
             if list[round(len(list)/2)-1]<value:
                 list = list[round(len(list)/2):]
-            elif list[round(len(list)/2)-1]>=value:
-                    list = list[0:round(len(list)/2)]
+            else:
+                list = list[0:round(len(list)/2)]
         if list[0]==value:
             return True 
         else:
             return False
 
-# hacer esto recursivo
+    def searchRecursive(self,list,value):
+        if len(list)==0:
+            return False
+        elif len(list)==1:
+            if list[0]==value:
+                return True
+            else: return False
+        else:
+            if list[round(len(list)/2)-1]<value:
+                list = list[round(len(list)/2):]
+            else:
+                list = list[0:round(len(list)/2)]
+        return self.searchRecursive(list, value)
+        
--- ejerciciosClase/pracRecursividadListas/tests.py
+++ ejerciciosClase/pracRecursividadListas/tests.py
@@ -1,7 +1,7 @@
 import unittest 
 from classList import SearchList
 
-class ValueSearchTestCase(unittest.TestCase):
+class ValueSearchProcedural(unittest.TestCase):
     def setUp(self):
         self.list = SearchList()
     def test_EmptyList(self):
@@ -35,5 +35,40 @@ class ValueSearchTestCase(unittest.TestCase):
     def test_MultipleValuesNotFoundInTheMiddle(self):
         self.assertFalse(self.list.searchProcedural([1,2,3,4,5,6,7,8,9,10,12,14], 13))
 
+class ValueSearchRecursive(unittest.TestCase):
+        def setUp(self):
+            self.list = SearchList()
+        def test_EmptyList(self):
+            self.assertFalse(self.list.searchRecursive([], 2))
+        def test_OneValue(self):
+            self.assertTrue(self.list.searchRecursive([1], 1))
+        def test_OneValueNotFound(self):
+            self.assertFalse(self.list.searchRecursive([1], 2))
+        def test_MultipleValuesMiddle(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14,15], 7))
+        def test_MultipleValuesMiddleRigth(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14,], 7))
+        def test_MultipleValuesMiddleLeft(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14,], 6))    
+        def test_MultipleValuesMax(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 12))
+        def test_MultipleValuesMin(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 2))
+        def test_MultipleValuesPosition0(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 1))
+        def test_MultipleValuesFinalPosition(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 14))
+        def test_MultipleValuesMixSliceMax(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 9))
+        def test_MultipleValuesMixSliceMin(self):
+            self.assertTrue(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 5))
+        def test_MultipleValuesNotFoundMax(self):
+            self.assertFalse(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 18))
+        def test_MultipleValuesNotFoundMin(self):
+            self.assertFalse(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], -2))
+        def test_MultipleValuesNotFoundInTheMiddle(self):
+            self.assertFalse(self.list.searchRecursive([1,2,3,4,5,6,7,8,9,10,12,14], 13))    
+
+    
 if __name__=='__main__':
     unittest.main()
