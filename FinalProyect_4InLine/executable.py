d31729b7d98626575297cb8ae67686009464f2ef
Ignacio687 <ignadavidchaves@gmail.com>
2022-09-07 12:29:46 -0300

    consoleExecutableCodeFinished

--- FinalProyect_4InLine/executable.py
+++ FinalProyect_4InLine/executable.py
@@ -1,12 +1,62 @@
-from game import FourInRow, NoAvailablePositionException, TieException, WinnerException
+from ast import Break
+from base64 import encode
+from inspect import EndOfBlock
+from logging import exception
+from game import FourInRow, NoAvailablePositionException, TieException, WinnerException, formatException, OutOfRangeException
 
 class Game():
 
     def __init__(self):
-        FourInRow()
+        self.game = FourInRow()
 
-    
+    def play(self):
+        print('Hello lest play Four In Row! \n\n')
+        gameStatus = False
+        while not gameStatus:
+            self.printBoard()
+            playerInstruction = input(f'\nPlayer {(self.game.turn)+1} select a Column(1-8/exit/reset):  ')
+            if playerInstruction in ('exit','Exit','EXIT'):
+                break
+            elif playerInstruction in ('reset', 'Reset', 'RESET'):
+                self.game.resetBoard()
+            else:
+                try:
+                    self.game.insertToken(int(playerInstruction)-1)
+                except (OutOfRangeException, formatException, ValueError):
+                    continue
+                except NoAvailablePositionException:
+                    print(f'\nTheres no more available positions in column {playerInstruction}\n')
+                except TieException:
+                    print('TIE! les try again')
+                    self.game.resetBoard()
+                except WinnerException:
+                    self.printBoard()
+                    print(f'\nPlayer {self.game.turn+1} winns!!!\n')
+                    while True:
+                        playerInstruction = input('Want to play again?(yes/no)  ')
+                        if  playerInstruction in ('yes', 'Yes', 'YES'):
+                            self.game.resetBoard()
+                            break
+                        elif playerInstruction in ('no', 'No', 'NO'):
+                            gameStatus = True
+                            break
+                    
 
+
+    def printBoard(self):
+        board = self.game.board
+        for line in range(1, len(board)+1):
+            print(f'  {line}  ', end='')
+        print('\n+'+('----+'*len(board)))
+        for line in range(0, len(board)):
+            for row in range(0, len(board[line])):
+                if board[line][row] != '':
+                    printValue = ' 🔴 ' if board[line][row] == 0 else ' 🔵 '
+                else:
+                    printValue = '    '
+                print (f'|{printValue}', end='')
+            print('|\n+'+('----+'*len(board)))
 
 if __name__=='__main__':
-    Game.play()
+    game = Game()
+    game.play()
--- FinalProyect_4InLine/executableTest.py
+++ FinalProyect_4InLine/executableTest.py
@@ -1,8 +1,36 @@
+from http.server import executable
 import unittest
+from unittest.mock import patch
 from executable import Game
 
 class ExecutableTestCase(unittest.TestCase):
 
     def setUp(self):
         self.game = Game()
-    
+
+    @patch('executable.Game.play')
+    @patch('executable.Game.printBoard')
+    def test_boardPrint(self, board, *args):
+        self.game.printBoard()
+        self.assertEqual(self.game.printBoard(), '''
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    |    |    |    |    |    |    |    |    |
+                                                    +----+----+----+----+----+----+----+----+
+                                                    ''')
+
+if __name__=='__main__':
+    unittest.main()
